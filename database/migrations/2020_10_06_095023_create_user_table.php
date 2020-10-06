a9b1a26f42fab1d8b9a7b50dd3999d3334418da8
Jasperator <55495886+Jasperator@users.noreply.github.com>
2020-10-06 14:07:34 +0200

    created migrations for users(students), company, internships and reviews

---
+++ database/migrations/2020_10_06_095023_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age');
+            $table->string('fase');
+            $table->text('preference');
+            $table->text('tools');
+            $table->string('location');
+            $table->text('bio');
+            $table->text('cv');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
