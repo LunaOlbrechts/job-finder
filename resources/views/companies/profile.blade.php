61f42c91c626716d03bb284a92f6a50f73c87ce4
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-10-30 09:33:39 +0100

    Added styling to pages

--- resources/views/companies/profile.blade.php
+++ resources/views/companies/profile.blade.php
@@ -3,23 +3,36 @@
 @section('content')
     <div class="container">
         <h1>{{ $company->name}}</h1>
-        <p>{{ $company->bio }}</p>
-        <p>{{ $company->projects }}</p>
-        <p>{{ $company->location }}</p>
-        <p>{{ $company->email }}</p>
-        <p>{{ $company->phone }}</p>
+        <div>
+            <h3>Biografie</h3>
+            <p>{{ $company->bio }}</p>
+        </div>
+        <div>
+            <h3>Projecten</h3>
+            <p>{{ $company->projects }}</p>
+        </div>
+        <div>
+            <h3>Locatie</h3>
+            <p>{{ $company->location }}</p>
+        </div>
+        <div>
+            <h3>Contact</h3>
+            <p>{{ $company->email }}</p>
+            <p>{{ $company->phone }}</p>
+        </div>
 
-        <h1>Nearest train station</h1>
-        <p>{{ $getNearestTrainStation[0]->name}}</p>
-
-
-        <h1>Internships</h1>
-        <p><a href="/companies/{{$company->id}}/filter"><h1>{{  __('Applications') }}</h1></a></p>
-        <br>
-
-        @foreach ($company->internships as $internship )
-            <a href="/internships/{{$internship->id}}"><h1>{{$internship->id}}</h1></a>
-            <p>{{$internship->bio}}</p>
-        @endforeach
+        <div>
+            <h3>Dichtstbijzijnde treinstation</h3>
+            <p>{{ $getNearestTrainStation[0]->name}}</p>
+        </div>
+        <div>
+            <h3>Aangebode stages</h3>
+                <p><a href="/companies/{{$company->id}}/filter"><p>{{  __('Applications') }}</p></a></p>
+            <br>
+            @foreach ($company->internships as $internship )
+                <a href="/internships/{{$internship->id}}"><p>{{$internship->id}}</p></a>
+                <p>{{$internship->bio}}</p>
+            @endforeach
+        </div>
     </div>
 @endsection
