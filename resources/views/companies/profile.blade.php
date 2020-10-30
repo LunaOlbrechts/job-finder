cb73df1906013d9e0aa1f0045632ae1c35fd5744
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-10-30 10:47:07 +0100

    Added link between internship and company

--- resources/views/companies/profile.blade.php
+++ resources/views/companies/profile.blade.php
@@ -37,19 +37,6 @@
             <a href="/internships/{{$internship->id}}"><h1>{{$internship->id}}</h1></a>
             <p>{{$internship->bio}}</p>
         @endforeach
-        <div>
-            <h3>Dichtstbijzijnde treinstation</h3>
-            <p>{{ $getNearestTrainStation[0]->name}}</p>
-        </div>
-        <div>
-            <h3>Aangebode stages</h3>
-                <p><a href="cfilter"><p>{{  __('Applications') }}</p></a></p>
-            <br>
-            @foreach ($company->internships as $internship )
-                <a href="/internships/{{$internship->id}}"><p>{{$internship->id}}</p></a>
-                <p>{{$internship->bio}}</p>
-            @endforeach
-        </div>
->>>>>>> 3d8e7298c07f41dfe41e610f22818c4a9317edb6
+
     </div>
 @endsection
