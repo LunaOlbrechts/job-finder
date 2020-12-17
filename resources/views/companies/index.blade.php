8be79cc4cf0e7941fa3bc5a8e6f1e0cc5671cb81
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-12-17 13:37:18 +0100

    SASS: Added styling on company index page

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="app">
-        <h1>Companies</h1>
-        <div class="container">
-            @foreach ($companies as $company)
-                <a href="/companies/{{ $company->id }}"><p>{{ $company->name}}</p></a>
-            @endforeach
+<div id="app">
+    <h1 class="page--title">Companies</h1>
+    <div class="container card--list">
+        @foreach ($companies as $company)
+        <div class="company--card-preview">
+            <div class="company--card-imgContainer">
+                <img class="company--card-logo" src="{{ $company->logo }}">
+            </div>
+            <a class="company--card-title" href="/companies/{{ $company->id }}">
+                <h3 class="company--card-name">{{ $company->name}}</h3>
+            </a>
+            <p class="company--card-text">{{ $company->bio }}</p>
+            <div class="company--card-button">
+                >
+            </div>
         </div>
-            <h1>{( title )}</h1>
+        @endforeach
     </div>
+    <h1>{( title )}</h1>
+</div>
 @endsection
