09e7b20818dcb789ec984ed7ac97479f664c3b3a
LunaOlbrechts <luna.olbrechts@gmail.com>
2021-01-06 21:47:19 +0100

    CRON

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('student:update')->everyMinute();
+        $schedule->command('trainStation:get')->everyMinute();
+        
+        //weeklyOn(1, '8:00');
     }
 
     /**
