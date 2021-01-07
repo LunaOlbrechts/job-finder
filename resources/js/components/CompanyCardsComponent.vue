cbdffb19196eb305a562e857155ada05e7d8686e
LunaOlbrechts <luna.olbrechts@gmail.com>
2021-01-07 19:50:56 +0100

    DEBUG: issues vuejs

--- resources/js/components/CompanyCardsComponent.vue
+++ resources/js/components/CompanyCardsComponent.vue
@@ -28,7 +28,7 @@ export default {
         getCompanies: function() {
             var that = this;
 
-            fetch("http://homestead.test/api/companies")
+            fetch("https://nextstep.chickenkiller.com/companies/api/companies")
                 .then(res => {
                     return res.json();
                 })
